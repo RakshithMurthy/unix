fa40ba3a8ace5391e32692bfed777cc1469dd12c
RakshithMurthy <35984675+RakshithMurthy@users.noreply.github.com>
2018-02-22 09:49:18 +0530

    Add files via upload

---
+++ arith.sh
@@ -0,0 +1,12 @@
+echo "Enter two number "
+read a b
+echo "1.add 2.sub 3.mult 4.div 5.exit"
+read choice
+case $choice in
+	        1) echo `expr $a + $b` ;;
+		        2) echo `expr $a - $b` ;;
+			        3) echo `expr $a \* $b` ;;
+				        4) echo `expr $a / $b` ;;
+					        5) echo "exit" ;;
+					esac
+
